32ccb20a6a6f0f97a259ed4375f24b656832bd16
jf1248 <user1234_987@hotmail.com>
2023-02-01 10:14:34 -0500

    tarea

---
+++ src/app/principal/principal.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+
+
+
+@NgModule({
+  declarations: [
+    UsuariosComponent,
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule
+  ],
+  exports: [
+    UsuariosComponent
+  ]
+})
+export class PrincipalModule { }
